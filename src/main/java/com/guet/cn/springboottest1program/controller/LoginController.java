032265f9adba9318d182ad80e1722eadbc819a30
zjj <watermelon152514@163.com>
2021-07-21 20:14:51 +0800

    框架二次变动

--- src/main/java/com/guet/cn/springboottest1program/controller/LoginController.java
+++ src/main/java/com/guet/cn/springboottest1program/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
+
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,6 +21,7 @@ public class LoginController {
 
     @RequestMapping("/HomePage")
     public String HomePage(){
+
         return "HomePage";
     }
 
