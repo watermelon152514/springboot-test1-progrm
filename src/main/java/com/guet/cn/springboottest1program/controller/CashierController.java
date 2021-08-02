9cc470c5b17f442c94d2bff79d454b09ed0bf440
1800800509 <2627274417@qq.com>
2021-08-02 17:44:54 +0800

    曹洋 提交任务 2021-08-02

--- src/main/java/com/guet/cn/springboottest1program/controller/CashierController.java
+++ src/main/java/com/guet/cn/springboottest1program/controller/CashierController.java
@@ -1,21 +1,23 @@
 package com.guet.cn.springboottest1program.controller;
 
+import com.guet.cn.springboottest1program.bean.Vip_information;
 import com.guet.cn.springboottest1program.service.CashierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/Cashier")
+//@RequestMapping("/Cashier")
 public class CashierController {
     @Autowired
     private CashierService cashierService;
 
-    @GetMapping("收银界面.html")
-    public String showCashier(){
-        return "收银界面";
-    }
+//    @GetMapping("收银界面.html")
+//    public String showCashier(){
+//        return "收银界面";
+//    }
 
     @GetMapping("收银.html")
     public String Cashier(){
@@ -40,5 +42,23 @@ public class CashierController {
     @GetMapping("收支.html")
     public String revenueAndExpenditure(){
         return "收支";
+    }
+
+    @GetMapping("searchTestjj")
+    public String searchTest(HttpServletRequest request){
+        return "收支";
+    }
+
+    @GetMapping("modifyVip")  //修改会员信息
+    public String modifyVip(Vip_information vip, HttpServletRequest request){
+        System.out.println(vip.getVIP_NAME());
+        System.out.println(vip.getVIP_PHONE());
+        System.out.println(vip.getBIRTHDAY());
+        System.out.println(vip.getCUSTOMER_SEX());
+        System.out.println(vip.getOPENG_TIME());
+        System.out.println(vip.getVIP_ID());
+        System.out.println(vip.getVIP_TYPE());
+        cashierService.insertVip(vip);
+        return "开卡.html";
     }
 }
--- src/main/java/com/guet/cn/springboottest1program/service/CashierService.java
+++ src/main/java/com/guet/cn/springboottest1program/service/CashierService.java
@@ -1,5 +1,11 @@
 package com.guet.cn.springboottest1program.service;
 
-public interface CashierService {
+import com.guet.cn.springboottest1program.bean.Project_bill;
+import com.guet.cn.springboottest1program.bean.Vip_information;
 
+import java.util.List;
+
+public interface CashierService {
+    List<Project_bill> searchTest(String searchTest);
+    void insertVip(Vip_information vip);
 }
